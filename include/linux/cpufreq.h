b87c42224eff1af9270df806e76dcc397bcbf921
AayushRd7 <aayush835@gmail.com>
2016-10-02 13:44:18 +0000

    lettuce:add smartmax governor
    Conflicts:
    	arch/arm64/configs/cyanogenmod_tomato-64_defconfig

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -1,3 +1,4 @@
+
 /*
  * linux/include/linux/cpufreq.h
  *
@@ -438,6 +439,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
 extern struct cpufreq_governor cpufreq_gov_elementalx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 #endif
 
 /*********************************************************************
