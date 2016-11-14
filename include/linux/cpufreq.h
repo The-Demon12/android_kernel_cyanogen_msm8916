e4fe858cebfe4a61e50d64d2e2cbf2cf6d590917
As007Akash <akashpathange.aaa47@gmail.com>
2016-11-14 15:03:01 +0530

    linux:cpufreq:Fix Missing Codes

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -225,7 +225,7 @@ struct cpufreq_driver {
 
 	/* optional */
 	int	(*bios_limit)	(int cpu, unsigned int *limit);
-
+	unsigned int (*getavg)	(struct cpufreq_policy *policy,unsigned int cpu);
 	int	(*exit)		(struct cpufreq_policy *policy);
 	int	(*suspend)	(struct cpufreq_policy *policy);
 	int	(*resume)	(struct cpufreq_policy *policy);
@@ -261,6 +261,8 @@ int cpufreq_register_driver(struct cpufreq_driver *driver_data);
 int cpufreq_unregister_driver(struct cpufreq_driver *driver_data);
 
 const char *cpufreq_get_current_driver(void);
+
+void cpufreq_notify_utilization(struct cpufreq_policy *policy,unsigned int load);
 
 static inline void cpufreq_verify_within_limits(struct cpufreq_policy *policy,
 		unsigned int min, unsigned int max)
