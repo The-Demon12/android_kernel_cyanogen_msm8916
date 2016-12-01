a3a3297027f9830404730863a006105120acb6aa
As007Akash <akashpathange.aaa47@gmail.com>
2016-12-01 07:58:20 +0530

    fixed drep

---
+++ build.sh
@@ -0,0 +1,66 @@
+
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/arch/arm64/boot/Image
+DTBTOOL=$KERNEL_DIR/dtbToolCM
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+# Modify the following variable if you want to build
+export CROSS_COMPILE=~/android-kernel/aarch64-linux-android-4.9-uber-master/bin/aarch64-linux-android-
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=As007
+export KBUILD_BUILD_HOST=StaySmart
+
+compile_kernel ()
+{
+echo -e "****************************************************************************"
+echo "                    "
+echo "                                        Compiling Kernel             "
+echo "                    "
+echo -e "****************************************************************************"
+make cyanogenmod_lettuce-64_defconfig
+make -j8
+if ! [ -a $KERN_IMG ];
+then
+echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
+exit 1
+fi
+$DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
+strip_modules
+}
+
+strip_modules ()
+{
+echo "Copying modules"
+rm $MODULES_DIR/*
+find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
+cd $MODULES_DIR
+echo "Stripping modules for size"
+$STRIP --strip-unneeded *.ko
+zip -9 modules *
+cd $KERNEL_DIR
+}
+
+case $1 in
+clean)
+make ARCH=arm64 -j8 clean mrproper
+rm -rf $KERNEL_DIR/arch/arm/boot/dt.img
+;;
+*)
+compile_kernel
+;;
+esac
+
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+echo -e "*********************************************************************"
+echo "                    "
+echo "                                        BUILD COMPLETE		      "
+echo "                            	           		   	      " 
+echo " "
+echo -e "*********************************************************************"
